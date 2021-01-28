0edd55805804094e2cb192d14cc7fa918b42b26d
Patrezze Alvarenga <patrezze@gmail.com>
2021-01-27 23:21:58 -0300

    WIP

---
+++ docker/config.php
@@ -0,0 +1,44 @@
+<?php
+
+define("BASE", (!empty(getenv('OT_BASE'))) ? getenv("OT_BASE") : "/opentask/");//Path to project
+//URI CONTROLLER
+define("URI_UNLINK_COUNT", 2);//Remove '/'
+define("URI_DEBUG", false);//Debug URI
+
+//DATABASE
+define("DB_HOST", "localhost");//Host database
+define("DB_USER", "root"); //User database
+define("DB_PASS", "");//Password database
+define("DB_NAME", "opentask"); //Database name
+
+//DEFAULT PASSWORD ON RESET
+define("DEFAULT_USER_PASS", "A123456Z");
+
+//DATETIME
+define("DATE_FORMAT", "d/m/Y");
+define("DATETIME_FORMAT", "d/m/Y H:i:s");
+define("TIMEZONE", "America/Sao_Paulo");
+
+//Do not change the code below
+//MODEL DEFAULT VALUES
+define("CONTROLLER", "HomeController");
+define("METHOD", "index");
+define("METHOD_HEADER", "Headerindex");//call inner <head></head>
+define("METHOD_HTTP", "Httpindex");//call befrore <!doctype>
+
+//UPLOAD
+define("FILE_PATH", "resources/files");
+define("IMAGE_PATH", "resources/images");
+define("MAX_FILE_SIZE", 50); //MB
+define("MAX_IMAGE_SIZE", 1);//MB
+define("ACCEPT_FORMAT", [
+  "image/gif",
+  "image/jpeg",
+  "image/png",
+  "application",
+  "application",
+  "application/zip",
+  "application/x-7z-compressed",
+  "application/x-zip-compressed"
+]);
+define("RENAME_FILE", true);
